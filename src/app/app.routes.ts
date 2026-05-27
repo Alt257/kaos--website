ab2b2b56875eb837424cb7ff6609fefd6e22604a
benoît <bra@synapsia.fr>
2026-05-26 20:54:45 -0400

    Add route for MatchImproMessages component and set as default route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MatchImproMessages } from './features/match-impro/match-impro-messages/match-impro-messages';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'match-impro',
+    component: MatchImproMessages,
+  },
+  {
+    path: '',
+    redirectTo: 'match-impro',
+    pathMatch: 'full',
+  },
+];
