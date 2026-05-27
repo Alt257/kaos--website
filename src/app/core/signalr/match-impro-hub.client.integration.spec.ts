a5394ca63839decf3fae4582b5ec428ff345615a
benoît <bra@synapsia.fr>
2026-05-27 15:46:18 -0400

    Add integration test for sending and receiving messages with dev backend

--- src/app/core/signalr/match-impro-hub.client.integration.spec.ts
+++ src/app/core/signalr/match-impro-hub.client.integration.spec.ts
@@ -3,7 +3,24 @@ import { describe, expect, it } from 'vitest';
 import { SignalRConnectionFactory } from './signalr-connection.factory';
 import { MatchImproHubClient } from './match-impro-hub.client';
 
+async function sendMessage(url: string,
+                           route: string,
+                           message: string) {
+  return await fetch(url + route, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      message: message,
+    }),
+  });
+}
+
 describe('MatchImproHubClient integration', () => {
+  const devUrl = 'http://127.0.0.1:8080';
+  const routeMessages = '/match-impro/messages';
+
   it('should connect to the backend SignalR hub', async () => {
     const client = new MatchImproHubClient(new SignalRConnectionFactory());
 
@@ -12,14 +29,20 @@ describe('MatchImproHubClient integration', () => {
     await client.stop();
   });
 
-  it('should receive message updates from the backend', async () => {
+  it('should send a message to the dev backend', async () => {
+    const response = await sendMessage(devUrl, routeMessages, 'Hello dev backend ?');
+
+    expect(response.status).toBe(200);
+  });
+
+  it('should receive message updates from the dev backend', async () => {
     const client = new MatchImproHubClient(new SignalRConnectionFactory());
 
     await client.start();
 
-    const messagePromise = firstValueFrom(
-      client.messages$.pipe(timeout({ first: 5000 })),
-    );
+    const messagePromise = firstValueFrom(client.messages$.pipe(timeout({ first: 5000 })));
+
+    await sendMessage(devUrl, routeMessages, 'Hello dev backend ?');
 
     const message = await messagePromise;
 
