e6f8ed42fa139d3c227188ec7a72156e3171a73d
benoît <benoit.rataux.pro@gmail.com>
2026-09-02 19:54:56 -0400

    #FEAT! app/config
    Add French locale support to the application
    
    - Register French locale data in `main.ts` using `registerLocaleData`
    - Provide `LOCALE_ID` with the value `fr` in `app.config.ts`

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,12 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    { provide: LOCALE_ID, useValue: 'fr' },
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+  ],
 };
